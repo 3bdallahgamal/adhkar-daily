d9976723cb426e87cb99fd20cfb51c446fad0444
3bdallahgamal <36746273+3bdallahgamal@users.noreply.github.com>
2026-09-23 23:16:59 +0300

    Refresh service worker cache for new home design

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="adhkar-daily-v12";
+const CACHE_NAME="adhkar-daily-v13";
 const ASSETS=[
   "./",
   "./index.html",
@@ -10,7 +10,8 @@ const ASSETS=[
   "./morning-scene.png",
   "./evening-scene.png",
   "./app-icon.svg",
-  "./assets/home-illustrations.jpg"
+  "./assets/home-illustrations.jpg",
+  "./assets/home-scenes.svg"
 ];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
